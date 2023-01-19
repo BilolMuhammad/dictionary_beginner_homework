8eac1c101615af683cf36f91d6446018226a0689
Bilol <mamatqulov@gmail.com>
2023-01-19 17:03:32 +0500

    count_users_with_age

--- count_users_with_age.py
+++ count_users_with_age.py
@@ -1,4 +1,4 @@
-def count_users_with_age(data:list, age:int) -> int:
+def count_users_with_age(data: list, age: int) -> int:
     """
     Return the number of users with a given age
 
@@ -8,4 +8,26 @@ def count_users_with_age(data:list, age:int) -> int:
     Returns:
         int: The number of users with the given age
     """
-    return 
+    sum_age = 0
+    for n in range(len(data)):
+        if data[n]['age'] == age:
+            sum_age += 1
+    return sum_age
+
+
+age = 17
+users = [
+    {
+        'name': 'Madina',
+        'age': 17
+    },
+    {
+        'name': 'Ozoda',
+        'age': 18
+    },
+    {
+        'name': 'Sevinch',
+        'age': 17
+    }
+]
+print(count_users_with_age(users, age))
