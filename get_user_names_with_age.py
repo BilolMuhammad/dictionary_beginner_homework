368e17b67dade6371554c5ccc094143abca41fbd
Bilol <mamatqulov@gmail.com>
2023-01-19 18:18:32 +0500

    get_user_names_with_age

--- get_user_names_with_age.py
+++ get_user_names_with_age.py
@@ -8,11 +8,14 @@ def get_user_names_with_age(data: list, age: int) -> list:
     Returns:
         list: A list of users with the given age
     """
-    idx = 0
+    idx = []
     for n in range(len(data)):
         if data[n]['age'] == age:
-            idx = n
-    return [data[idx]['name']]
+            idx.append(n)
+    user = []
+    for i in idx:
+        user.append(data[i]['name'])
+    return user
 
 
 age = 17
@@ -23,6 +26,10 @@ data = [
     },
     {
         'name': 'Madina',
+        'age': 17
+    },
+    {
+        'name': 'Kamola',
         'age': 17
     }
 ]
