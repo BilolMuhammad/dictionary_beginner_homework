891ee8141eaa3525d5a4caf0ba42527b48504841
Bilol <mamatqulov@gmail.com>
2023-01-19 17:19:00 +0500

    max_key

--- find_max_key.py
+++ find_max_key.py
@@ -6,4 +6,17 @@ def find_max_key(data: dict):
     Returns:
         int: The maximum key in the dictionary.
     """
-    return 
+    max = 0
+    for k in data:
+        if k > max:
+            max = k
+    return max
+
+
+dic = {
+    3.3: 'sdfsd',
+    2.2222: 'sdfsd',
+    11.111: '',
+    -3: 'sdfds',
+}
+print(find_max_key(dic))
